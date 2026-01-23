9333dca36b449ef2f4a47d8a80b2f08cacd105e9
Souvik Mandal <souvikmandals10@gmail.com>
2026-01-23 16:08:24 +0530

    feat: enhance RegistrationScreen with user input validation and error handling

--- lib/features/auth/presentation/registration_screen.dart
+++ lib/features/auth/presentation/registration_screen.dart
@@ -2,6 +2,7 @@ import 'package:autoroutine/features/auth/cubit/auth_cubit.dart';
 import 'package:autoroutine/features/auth/cubit/auth_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 class RegistrationScreen extends StatefulWidget {
   const RegistrationScreen({super.key});
 
@@ -32,6 +33,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
         context,
       ).showSnackBar(const SnackBar(content: Text('All fields are required ')));
     }
+    context.read<AuthCubit>().register(email, password, name);
 
     if (password.length < 6) {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -46,79 +48,117 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title : Center(child: const Text('Create Account', style: TextStyle(fontWeight: FontWeight.bold),))),
-      body:
-        Padding(
-          padding: EdgeInsetsGeometry.all(24),
-          child: BlocConsumer<AuthCubit, AuthState>(
-            listener: (context, state) {
-              if (state is AuthError) {
-                ScaffoldMessenger.of(
-                  context,
-                ).showSnackBar(SnackBar(content: Text(state.message)));
-              }
-            },
-
-            builder: (context, state) {
-              final isLoading = state is AuthLoading;
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                
-                  const SizedBox(height: 50),
-
-                  TextField(
-                    controller: _namecontroller,
-                    keyboardType: TextInputType.name,
-                    decoration: InputDecoration(
-                      label: Text("Name", style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),),
-                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(8))
-                    ),
-                  ),
-
-                  const SizedBox(height: 14),  
-
-                  TextField(
-                    controller: _emailcontroller,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      label: Text("Email", style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),),
-                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(8))
-                    ),
-                  ),
-
-                  const SizedBox(height: 14),  
-
-                  TextField(
-                    controller: _passwordcontroller,
-                    keyboardType: TextInputType.visiblePassword,
-                    decoration: InputDecoration(
-                      label: Text("Password", style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),),
-                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
-                    ),
-                  ),
-
-                  const SizedBox(height: 14,),
-                  
-                  SizedBox(
-                    width: 250,
-                    height: 60,
-                    child: ElevatedButton(
-                      onPressed: isLoading ? null : _onRegisterPressed,
-                      child: isLoading 
-                      ? CircularProgressIndicator(
-                        backgroundColor: const Color.fromARGB(255, 226, 216, 216),
-                        strokeWidth: 2,
-                      )
-                      : Center(child: const Text('Register', style: TextStyle(fontSize: 20),))
-                      ),
-                  ),
-
-                ],
-              );
-            },
+      appBar: AppBar(
+        title: Center(
+          child: const Text(
+            'Create Account',
+            style: TextStyle(fontWeight: FontWeight.bold),
           ),
         ),
+      ),
+      body: Padding(
+        padding: EdgeInsetsGeometry.all(24),
+        child: BlocConsumer<AuthCubit, AuthState>(
+          listener: (context, state) {
+            if (state is AuthError) {
+              ScaffoldMessenger.of(
+                context,
+              ).showSnackBar(SnackBar(content: Text(state.message)));
+            }
+          },
+
+          builder: (context, state) {
+            final isLoading = state is AuthLoading;
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 50),
+
+                TextField(
+                  controller: _namecontroller,
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(
+                    label: Text(
+                      "Name",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 14),
+
+                TextField(
+                  controller: _emailcontroller,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    label: Text(
+                      "Email",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 14),
+
+                TextField(
+                  controller: _passwordcontroller,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: InputDecoration(
+                    label: Text(
+                      "Password",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+
+                const SizedBox(height: 14),
+
+                SizedBox(
+                  width: 250,
+                  height: 60,
+                  child: ElevatedButton(
+                    onPressed: isLoading ? null : _onRegisterPressed,
+                    child: isLoading
+                        ? CircularProgressIndicator(
+                            backgroundColor: const Color.fromARGB(
+                              255,
+                              226,
+                              216,
+                              216,
+                            ),
+                            strokeWidth: 2,
+                          )
+                        : Center(
+                            child: const Text(
+                              'Register',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
     );
   }
 }
